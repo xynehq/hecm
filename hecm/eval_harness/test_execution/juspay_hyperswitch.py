16b338b7c589816437f4efee24104bbc5366e4d7
Ubuntu <azureuser@hyperswitch-2.1k0fm4dkoltuzn2napqogmh5ae.hx.internal.cloudapp.net>
2025-10-16 14:19:30 +0000

    update: docstrings

--- hecm/eval_harness/test_execution/juspay_hyperswitch.py
+++ hecm/eval_harness/test_execution/juspay_hyperswitch.py
@@ -13,20 +13,25 @@ class JuspayHyperswitchSandboxedTestExecutor(BaseSandboxedExecutor):
 
     !!! example
         ```python
-        import rich
         import weave
         from datasets import load_dataset
 
         from hecm.dataset_generation.schemas import CodingAgentDataPoint
         from hecm.eval_harness.test_execution import JuspayHyperswitchSandboxedTestExecutor
 
-        weave.init(project_name="hyperswitch")
 
-        dataset = load_dataset("geekyrakshit/rust-dev", split="train")
-        data_point = CodingAgentDataPoint.model_validate(dataset[0])
-        executor = JuspayHyperswitchSandboxedTestExecutor()
-        results = executor.execute(data_point)
-        executor.cleanup()
+        @weave.op
+        def test_cypress_execution():
+            dataset = load_dataset("geekyrakshit/rust-dev", split="train")
+            data_point = CodingAgentDataPoint.model_validate(dataset[0])
+            executor = JuspayHyperswitchSandboxedTestExecutor(show_output_logs=True)
+            results = executor.execute(data_point)
+            executor.cleanup()
+            return results
+
+
+        weave.init(project_name="hyperswitch")
+        test_cypress_execution()
         ```
 
     Args:
@@ -90,6 +95,29 @@ class JuspayHyperswitchSandboxedTestExecutor(BaseSandboxedExecutor):
 
 class JuspayHyperswitchLocalTestExecutor(BaseLocalExecutor):
     """Executes tests for Juspay Hyperswitch in the local environment.
+
+    !!! example
+        ```python
+        import weave
+        from datasets import load_dataset
+
+        from hecm.dataset_generation.schemas import CodingAgentDataPoint
+        from hecm.eval_harness.test_execution import JuspayHyperswitchLocalTestExecutor
+
+
+        @weave.op
+        def test_cypress_execution():
+            dataset = load_dataset("geekyrakshit/rust-dev", split="train")
+            data_point = CodingAgentDataPoint.model_validate(dataset[0])
+            executor = JuspayHyperswitchLocalTestExecutor(show_output_logs=True)
+            results = executor.execute(data_point)
+            executor.cleanup()
+            return results
+
+
+        weave.init(project_name="hyperswitch")
+        test_cypress_execution()
+        ```
 
     Args:
         environment (Dict[str, str]): Environment variables to set (default: None)
